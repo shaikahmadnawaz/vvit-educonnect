44a7044f3f6a246b837e596b5d7103aa5258aef4
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-20 00:29:58 +0530

    backend initial commit

---
+++ server/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import multer from "multer";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 25 * 1024 * 1024,
+  },
+});
+
+const port = process.env.PORT || 5000;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+// Start the server
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
